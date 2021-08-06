d984a06aa40cf8b143ba9d27bc36e0610750fe69
Ishan Achinta <ishan.achinta1@gmail.com>
2021-08-06 13:51:29 +0530

    First Commit

---
+++ src/test/java/com/example/covidapp/CovidappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
